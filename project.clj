090f5cc2fa654cfc11ad1f4b5a37df42cbfc3435
Arvind <arvindc@thoughtworks.com>
2013-03-14 21:01:56 +0530

    Bringing in Hiccup and Enlive.

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Unknown description"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.5"]
-				  [hiccup "1.0.0"]]
+				 [hiccup "1.0.0"]
+				 [enlive "1.1.1"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler hello.route/app}
   :profiles
